36ae888daee8e22e6a06cb0d44c88bcaab259b10
JayJay1024 <lygm1062@gmail.com>
2023-07-06 01:19:29 +0800

    network config and url type

---
+++ src/types/chain.ts
@@ -0,0 +1,55 @@
+export enum ChainID {
+  // PANGOLIN = 43,
+  CRAB = 44,
+  // PANGORO = 45,
+  DARWINIA = 46,
+}
+
+export interface RpcMeta {
+  name?: string;
+  url: string;
+}
+
+interface NativeToken {
+  symbol: string;
+  decimals: number;
+  logoPath: string;
+}
+
+interface KtonToken extends NativeToken {
+  address: string;
+}
+
+interface Contract {
+  abiPath: string;
+  address: string;
+}
+
+interface Substrate {
+  rpc: {
+    wss: string;
+    https: string;
+  };
+  graphql: {
+    endpoint: string;
+  };
+}
+
+export interface ChainConfig {
+  name: string;
+  chainId: ChainID;
+  nativeToken: NativeToken;
+  ktonToken?: KtonToken;
+  rpcMetas: RpcMeta[];
+  explorer: {
+    name: string;
+    url: string;
+  };
+  contract: {
+    deposit: Contract;
+    staking: Contract;
+  };
+  secondsPerBlock: number;
+  substrate: Substrate;
+  isTestNet?: boolean;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from "./chain";
+export * from "./url";
