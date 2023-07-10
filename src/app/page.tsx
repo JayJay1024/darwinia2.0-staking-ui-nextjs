ce099ff068a484a50257519eaf12f086deb5e0fc
JayJay1024 <lygm1062@gmail.com>
2023-07-10 16:22:18 +0800

    fix home page height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Login = dynamic(() => import("@/components/login"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="p-large pt-0 h-full">
+    <div className="p-large pt-0 home-page">
       <div className="container mx-auto bg-component h-full flex flex-col justify-center items-center gap-6">
         <Login />
       </div>
