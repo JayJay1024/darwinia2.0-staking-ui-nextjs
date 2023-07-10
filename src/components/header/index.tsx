e6bbcff3ef1301fb69025a42e2c157349a382fc3
JayJay1024 <lygm1062@gmail.com>
2023-07-10 15:37:43 +0800

    fix header background color

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ const User = dynamic(() => import("./user"), { ssr: false });
 
 export default function Header({ className }: { className: string }) {
   return (
-    <div className={`${className} flex items-center px-large`}>
+    <div className={`${className} flex items-center px-large bg-app-black`}>
       <div className="container mx-auto flex items-center justify-between">
         <div>
           <User />
