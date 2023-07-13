3c5638b70b3467012d55b786abdf8c884f32b2ca
JayJay1024 <lygm1062@gmail.com>
2023-07-13 18:20:50 +0800

    improve action button

--- src/components/delegate-list.tsx
+++ src/components/delegate-list.tsx
@@ -114,7 +114,7 @@ function ActionButton({ action, ...rest }: ButtonHTMLAttributes<HTMLButtonElemen
     <button
       type="button"
       {...rest}
-      className="inline-flex h-[14px] w-[14px] shrink-0 items-center justify-center border border-white/40"
+      className="inline-flex h-[14px] w-[14px] shrink-0 items-center justify-center border border-white/40 transition-transform hover:scale-105 active:scale-95"
     >
       <span className="text-xs">{action === "bond" ? "+" : "-"}</span>
     </button>
