3d48ecbee3e83415295dbe233518eba65f55fc1c
JayJay1024 <lygm1062@gmail.com>
2023-07-17 18:34:07 +0800

    fix home page padding top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Login = dynamic(() => import("@/components/login"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="home-page p-large pt-0">
+    <div className="home-page p-large pt-0 lg:pt-large">
       <div className="container mx-auto flex h-full flex-col items-center justify-center gap-6 bg-component">
         <Login />
       </div>
