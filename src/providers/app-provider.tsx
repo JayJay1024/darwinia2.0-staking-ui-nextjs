26409ce8d309c4f493126fc3d23a5c13d5171560
JayJay1024 <lygm1062@gmail.com>
2023-07-17 20:33:16 +0800

    custom rpc

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -1,35 +1,48 @@
 "use client";
 
 import { ChainID, UrlParamsKey, StoreKey, RpcMeta } from "@/types";
-import { getChainConfig } from "@/utils";
+import { getChainConfig, getChainConfigs } from "@/utils";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Dispatch, PropsWithChildren, SetStateAction, createContext, useCallback, useEffect, useState } from "react";
+import { PropsWithChildren, createContext, useCallback, useEffect, useState } from "react";
+
+const defaultChainConfig = getChainConfig(ChainID.DARWINIA);
 
 interface AppCtx {
   activeChain: ChainID;
   setActiveChain: (chainId: ChainID) => void;
   activeRpc: RpcMeta;
-  setActiveRpc: Dispatch<SetStateAction<RpcMeta>>;
+  setActiveRpc: (rpcMeta: RpcMeta) => void;
 }
 
 export const AppContext = createContext<AppCtx>({
-  activeChain: ChainID.DARWINIA,
+  activeChain: defaultChainConfig.chainId,
   setActiveChain: () => undefined,
   activeRpc: { url: "" },
   setActiveRpc: () => undefined,
 });
 
 export function AppProvider({ children }: PropsWithChildren<unknown>) {
-  const [activeChain, _setActiveChain] = useState<AppCtx["activeChain"]>(ChainID.DARWINIA);
-  const [activeRpc, _setActiveRpc] = useState<AppCtx["activeRpc"]>({ url: "" });
+  const [activeChain, _setActiveChain] = useState<AppCtx["activeChain"]>(defaultChainConfig.chainId);
+  const [activeRpc, _setActiveRpc] = useState<AppCtx["activeRpc"]>(defaultChainConfig.rpcMetas[0]);
 
   const searchParams = useSearchParams();
   const router = useRouter();
 
-  const setActiveChain = useCallback((chainId: ChainID) => {
-    _setActiveChain(chainId);
-    localStorage.setItem(StoreKey.ACTIVE_CHAIN, chainId.toString());
-  }, []);
+  const setActiveChain = useCallback(
+    (chainId: ChainID) => {
+      _setActiveChain(chainId);
+      localStorage.setItem(StoreKey.ACTIVE_CHAIN, chainId.toString());
+
+      const chainConfig = getChainConfig(chainId);
+      _setActiveRpc(chainConfig.rpcMetas[0]);
+
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(UrlParamsKey.NETWORK, chainConfig.name);
+
+      router.push(`?${params.toString()}`);
+    },
+    [router, searchParams]
+  );
 
   const setActiveRpc = useCallback(
     (rpcMeta: RpcMeta) => {
@@ -47,22 +60,22 @@ export function AppProvider({ children }: PropsWithChildren<unknown>) {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const urlChain = urlSearchParams.get(UrlParamsKey.NETWORK);
     const urlRpc = urlSearchParams.get(UrlParamsKey.RPC);
-    let initChain = ChainID.DARWINIA;
+    let initChain = defaultChainConfig;
 
     if (urlChain) {
-      for (const chainId of Object.values(ChainID)) {
-        if (urlChain === chainId.toString()) {
-          initChain = chainId as ChainID;
+      for (const chainConfig of getChainConfigs()) {
+        if (urlChain === chainConfig.name) {
+          initChain = chainConfig;
         }
       }
     }
-    _setActiveChain(initChain);
+    _setActiveChain(initChain.chainId);
     localStorage.setItem(StoreKey.ACTIVE_CHAIN, initChain.toString());
 
     if (urlRpc) {
-      setActiveRpc({ url: decodeURIComponent(urlRpc) });
+      _setActiveRpc({ url: decodeURIComponent(urlRpc) });
     } else {
-      setActiveRpc(getChainConfig(initChain).rpcMetas.at(0) || { url: "" });
+      _setActiveRpc(getChainConfig(initChain.chainId).rpcMetas.at(0) || { url: "" });
     }
   }, []);
 
