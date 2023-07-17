c6dbe0eb97efdc1663d88363d4331fe267ebebc3
JayJay1024 <lygm1062@gmail.com>
2023-07-17 20:20:03 +0800

    rpc url params

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -2,6 +2,7 @@
 
 import { ChainID, UrlParamsKey, StoreKey, RpcMeta } from "@/types";
 import { getChainConfig } from "@/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Dispatch, PropsWithChildren, SetStateAction, createContext, useCallback, useEffect, useState } from "react";
 
 interface AppCtx {
@@ -20,12 +21,27 @@ export const AppContext = createContext<AppCtx>({
 
 export function AppProvider({ children }: PropsWithChildren<unknown>) {
   const [activeChain, _setActiveChain] = useState<AppCtx["activeChain"]>(ChainID.DARWINIA);
-  const [activeRpc, setActiveRpc] = useState<AppCtx["activeRpc"]>({ url: "" });
+  const [activeRpc, _setActiveRpc] = useState<AppCtx["activeRpc"]>({ url: "" });
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   const setActiveChain = useCallback((chainId: ChainID) => {
     _setActiveChain(chainId);
     localStorage.setItem(StoreKey.ACTIVE_CHAIN, chainId.toString());
   }, []);
+
+  const setActiveRpc = useCallback(
+    (rpcMeta: RpcMeta) => {
+      _setActiveRpc(rpcMeta);
+
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(UrlParamsKey.RPC, encodeURIComponent(rpcMeta.url));
+
+      router.push(`?${params.toString()}`);
+    },
+    [router, searchParams]
+  );
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search);
