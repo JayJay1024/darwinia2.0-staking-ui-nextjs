779e243d9bc4a64d2468468e620d22d3bc9e1fab
JayJay1024 <lygm1062@gmail.com>
2023-07-17 19:51:00 +0800

    custom rpc

--- src/components/custom-rpc/index.tsx
+++ src/components/custom-rpc/index.tsx
@@ -1,11 +1,66 @@
+"use client";
+
+import { useApp } from "@/hooks";
+import { RpcMeta, UrlParamsKey } from "@/types";
+import { getChainConfig } from "@/utils";
+import { useFloating, offset, useTransitionStyles, useClick, useDismiss, useInteractions } from "@floating-ui/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import RpcSelector from "./rpc-selector";
 
 export default function CustomRpc() {
+  const [rpcMetas, setRpcMetas] = useState<RpcMeta[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const { activeChain } = useApp();
+
+  const { refs, context, floatingStyles } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: "bottom-end",
+    middleware: [offset(10)],
+  });
+
+  const { styles, isMounted } = useTransitionStyles(context, {
+    initial: { transform: "translateY(-20px)", opacity: 0 },
+    open: { transform: "translateY(0)", opacity: 1 },
+    close: { transform: "translateY(-20px)", opacity: 0 },
+  });
+
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss]);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const rpcParam = searchParams.get(UrlParamsKey.RPC);
+
+    const chainConfig = getChainConfig(activeChain);
+    const rpcParamUrl = rpcParam ? decodeURIComponent(rpcParam) : null;
+
+    if (chainConfig.rpcMetas.some(({ url }) => url === rpcParamUrl)) {
+      setRpcMetas(chainConfig.rpcMetas);
+    } else {
+      setRpcMetas(chainConfig.rpcMetas.concat(rpcParamUrl ? { url: rpcParamUrl } : []));
+    }
+  }, [activeChain]);
+
   return (
     <>
-      <button className="inline-flex h-9 w-9 items-center justify-center border border-primary text-sm font-light text-white transition-opacity hover:opacity-80 active:opacity-60">
+      <button
+        className="inline-flex h-9 w-9 items-center justify-center border border-primary text-sm font-light text-white transition-opacity hover:opacity-80 active:opacity-60"
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
         <Image width={16} height={16} alt="Custom rpc" src="/images/setting.svg" />
       </button>
+
+      {isMounted && (
+        <div style={floatingStyles} ref={refs.setFloating} {...getFloatingProps()} className="z-10">
+          <div style={styles}>
+            <RpcSelector rpcMetas={rpcMetas} setRpcMetas={setRpcMetas} onClose={() => setIsOpen(false)} />
+          </div>
+        </div>
+      )}
     </>
   );
 }
---
+++ src/components/custom-rpc/rpc-selector.tsx
@@ -0,0 +1,84 @@
+import { useApp } from "@/hooks";
+import { RpcMeta } from "@/types";
+import { Dispatch, SetStateAction, useState } from "react";
+import AddRpcModal from "./add-rpc-modal";
+
+export default function RpcSelector({
+  rpcMetas,
+  setRpcMetas,
+  onClose = () => undefined,
+}: {
+  rpcMetas: RpcMeta[];
+  setRpcMetas: Dispatch<SetStateAction<RpcMeta[]>>;
+  onClose?: () => void;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const { activeRpc, setActiveRpc } = useApp();
+
+  return (
+    <>
+      <div className="flex w-60 flex-col gap-middle border border-primary bg-component p-5">
+        <div className="max-h-[25vh] overflow-y-auto">
+          <div className="flex flex-col gap-middle">
+            {rpcMetas.map((rpcMeta) => (
+              <RpcItem
+                key={rpcMeta.url}
+                activeRpc={activeRpc}
+                rpcMeta={rpcMeta}
+                onSelect={(value) => {
+                  setActiveRpc(value);
+                  onClose();
+                }}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex h-4 items-center gap-middle">
+          <div className="h-[1px] w-full bg-white/20" />
+          <span className="text-xs font-bold text-white">or</span>
+          <div className="h-[1px] w-full bg-white/20" />
+        </div>
+
+        <button
+          className="flex w-full items-center justify-center border border-primary bg-transparent py-2 transition-opacity hover:opacity-80 active:opacity-60"
+          onClick={() => setIsOpen(true)}
+        >
+          <span className="text-xs font-bold text-white">Add Custom Endpoint</span>
+        </button>
+      </div>
+
+      <AddRpcModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onSave={(value) => setRpcMetas((prev) => (prev.some(({ url }) => url === value.url) ? prev : [...prev, value]))}
+      />
+    </>
+  );
+}
+
+function RpcItem({
+  activeRpc,
+  rpcMeta,
+  onSelect,
+}: {
+  activeRpc: RpcMeta;
+  rpcMeta: RpcMeta;
+  onSelect: (rpcMeta: RpcMeta) => void;
+}) {
+  return (
+    <div
+      className={`flex gap-middle p-small transition hover:cursor-pointer hover:opacity-80 active:opacity-60 ${
+        activeRpc.url === rpcMeta.url ? "bg-primary/20" : "bg-white/20"
+      }`}
+      onClick={() => {
+        onSelect(rpcMeta);
+      }}
+    >
+      <div
+        className={`w-[3px] shrink-0 transition-colors ${activeRpc.url === rpcMeta.url ? "bg-primary" : "bg-white/20"}`}
+      />
+      <span className="break-all text-xs font-bold">via {rpcMeta.name ?? rpcMeta.url}</span>
+    </div>
+  );
+}
