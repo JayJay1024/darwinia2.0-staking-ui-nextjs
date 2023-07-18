9329ccf39bea45d940a34cc21502aec8f9d32bbf
JayJay1024 <lygm1062@gmail.com>
2023-07-18 15:07:57 +0800

    use active rpc

--- src/providers/rainbow-provider.tsx
+++ src/providers/rainbow-provider.tsx
@@ -11,67 +11,67 @@ import { getChainConfigs } from "@/utils";
 import { PropsWithChildren, useEffect, useState } from "react";
 import { useApp } from "@/hooks";
 
-const { chains, publicClient } = configureChains(
-  getChainConfigs().map(({ chainId, name, nativeToken, rpcMetas, explorer }) => ({
-    id: chainId,
-    name,
-    network: name.toLowerCase().split(" ").join("-"),
-    nativeCurrency: {
-      name: nativeToken.symbol,
-      symbol: nativeToken.symbol,
-      decimals: nativeToken.decimals,
-    },
-    rpcUrls: {
-      default: {
-        http: rpcMetas.at(0)?.url.startsWith("http") ? [rpcMetas[0].url] : [],
-        webSocket: rpcMetas.at(0)?.url.startsWith("ws") ? [rpcMetas[0].url] : [],
-      },
-      public: {
-        http: rpcMetas.at(0)?.url.startsWith("http") ? [rpcMetas[0].url] : [],
-        webSocket: rpcMetas.at(0)?.url.startsWith("ws") ? [rpcMetas[0].url] : [],
-      },
-    },
-    blockExplorers: {
-      default: {
-        url: explorer.url,
-        name: explorer.name,
-      },
-    },
-  })),
-  [publicProvider()]
-);
-
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "";
-
-const { wallets } = getDefaultWallets({
-  appName: APP_NAME_CONF,
-  projectId,
-  chains,
-});
-
-const connectors = connectorsForWallets([
-  ...wallets,
-  {
-    groupName: "More",
-    wallets: [
-      argentWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-      imTokenWallet({ projectId, chains }),
-      okxWallet({ projectId, chains }),
-    ],
-  },
-]);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
 
 export function RainbowProvider({ children }: PropsWithChildren<unknown>) {
   const [mounted, setMounted] = useState(true); // temporarity set to true
-  const { activeChain } = useApp();
+  const { activeChain, activeRpc } = useApp();
+
+  const { chains, publicClient } = configureChains(
+    getChainConfigs().map(({ chainId, name, nativeToken, explorer }) => ({
+      id: chainId,
+      name,
+      network: name.toLowerCase().split(" ").join("-"),
+      nativeCurrency: {
+        name: nativeToken.symbol,
+        symbol: nativeToken.symbol,
+        decimals: nativeToken.decimals,
+      },
+      rpcUrls: {
+        default: {
+          http: activeRpc.url.startsWith("http") ? [activeRpc.url] : [],
+          webSocket: activeRpc.url.startsWith("ws") ? [activeRpc.url] : [],
+        },
+        public: {
+          http: activeRpc.url.startsWith("http") ? [activeRpc.url] : [],
+          webSocket: activeRpc.url.startsWith("ws") ? [activeRpc.url] : [],
+        },
+      },
+      blockExplorers: {
+        default: {
+          url: explorer.url,
+          name: explorer.name,
+        },
+      },
+    })),
+    [publicProvider()]
+  );
+
+  const { wallets } = getDefaultWallets({
+    appName: APP_NAME_CONF,
+    projectId,
+    chains,
+  });
+
+  const connectors = connectorsForWallets([
+    ...wallets,
+    {
+      groupName: "More",
+      wallets: [
+        argentWallet({ projectId, chains }),
+        trustWallet({ projectId, chains }),
+        ledgerWallet({ projectId, chains }),
+        imTokenWallet({ projectId, chains }),
+        okxWallet({ projectId, chains }),
+      ],
+    },
+  ]);
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
 
   useEffect(() => setMounted(true), []);
 
