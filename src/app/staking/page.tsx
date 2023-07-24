46a7ad30a03235c20e26377d304cd6147025f2f0
JayJay1024 <lygm1062@gmail.com>
2023-07-24 18:39:13 +0800

    graphql provider

--- src/app/staking/page.tsx
+++ src/app/staking/page.tsx
@@ -1,4 +1,4 @@
-import { ApiProvider, StakingProvider } from "@/providers";
+import { ApiProvider, GraphQLProvider, StakingProvider } from "@/providers";
 import dynamic from "next/dynamic";
 
 const StakingDashboard = dynamic(() => import("@/components/staking-dashboard"), { ssr: false });
@@ -8,7 +8,9 @@ export default function Staking() {
     <div className="flex flex-col gap-10 p-large lg:container lg:mx-auto lg:px-0 lg:pb-5 lg:pt-8">
       <ApiProvider>
         <StakingProvider>
-          <StakingDashboard />
+          <GraphQLProvider>
+            <StakingDashboard />
+          </GraphQLProvider>
         </StakingProvider>
       </ApiProvider>
     </div>
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -3,3 +3,4 @@ export * from "./app-provider";
 export * from "./staking-provider";
 export * from "./api-provider";
 export * from "./notification-provider";
+export * from "./graphql-provider";
