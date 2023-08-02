3046f76c497e283f8eecb0b04c4486e8d8f7ae64
JayJay1024 <lygm1062@gmail.com>
2023-08-02 18:13:14 +0800

    button busy status

--- src/components/ensure-match-network-button.tsx
+++ src/components/ensure-match-network-button.tsx
@@ -27,87 +27,92 @@ import {
   useTransitionStyles,
 } from "@floating-ui/react";
 
-export default forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement> & { kind?: "scale" | "opacity" }>(
-  function EnsureMatchNetworkButton({ children, onClick, kind = "opacity", ...rest }, ref) {
-    const { activeChain } = useApp();
-    const { chain } = useNetwork();
-    const { switchNetwork } = useSwitchNetwork();
+export default forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement> & { kind?: "scale" | "opacity"; busy?: boolean }
+>(function EnsureMatchNetworkButton({ children, onClick, kind = "opacity", disabled, busy, ...rest }, ref) {
+  const { activeChain } = useApp();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
 
-    const isMatch = useMemo(() => chain?.id === activeChain, [chain?.id, activeChain]);
+  const isMatch = useMemo(() => chain?.id === activeChain, [chain?.id, activeChain]);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const arrowRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const arrowRef = useRef(null);
 
-    const { refs, floatingStyles, context } = useFloating({
-      open: isOpen,
-      onOpenChange: setIsOpen,
-      placement: "top",
-      middleware: [offset(10), flip(), shift(), arrow({ element: arrowRef })],
-      whileElementsMounted: autoUpdate,
-    });
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: "top",
+    middleware: [offset(10), flip(), shift(), arrow({ element: arrowRef })],
+    whileElementsMounted: autoUpdate,
+  });
 
-    const hover = useHover(context, { move: false, enabled: !isMatch, handleClose: safePolygon() });
-    const role = useRole(context);
-    const { getReferenceProps, getFloatingProps } = useInteractions([hover, role]);
+  const hover = useHover(context, { move: false, enabled: !isMatch, handleClose: safePolygon() });
+  const role = useRole(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover, role]);
 
-    const { isMounted, styles } = useTransitionStyles(context, {
-      initial: { transform: "scale(0.5)", opacity: 0 },
-      open: { transform: "scale(1)", opacity: 1 },
-      close: { transform: "scale(0.5)", opacity: 0 },
-    });
+  const { isMounted, styles } = useTransitionStyles(context, {
+    initial: { transform: "scale(0.5)", opacity: 0 },
+    open: { transform: "scale(1)", opacity: 1 },
+    close: { transform: "scale(0.5)", opacity: 0 },
+  });
 
-    useImperativeHandle(ref, () => refs.reference.current as HTMLButtonElement, [refs]);
+  useImperativeHandle(ref, () => refs.reference.current as HTMLButtonElement, [refs]);
 
-    const handleClick = useCallback<MouseEventHandler<HTMLButtonElement>>(
-      (e) => {
-        if (isMatch && onClick) {
-          return onClick(e);
-        }
-        return undefined;
-      },
-      [isMatch, onClick]
-    );
+  const handleClick = useCallback<MouseEventHandler<HTMLButtonElement>>(
+    (e) => {
+      if (isMatch && onClick) {
+        return onClick(e);
+      }
+      return undefined;
+    },
+    [isMatch, onClick]
+  );
 
-    return (
-      <>
-        {kind === "opacity" ? (
-          <OpacityButton
-            ref={refs.setReference}
-            onClick={handleClick}
-            {...rest}
-            {...getReferenceProps()}
-            data-disabled={!isMatch}
-          >
-            {children}
-          </OpacityButton>
-        ) : (
-          <ScaleButton
-            ref={refs.setReference}
-            onClick={handleClick}
-            {...rest}
-            {...getReferenceProps()}
-            data-disabled={!isMatch}
-          >
-            {children}
-          </ScaleButton>
-        )}
-        {isMounted && (
-          <div ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
-            <FloatingArrow ref={arrowRef} style={styles} context={context} fill="#FF0083" />
-            <div style={styles} className="w-[70vw] border border-primary bg-component  p-middle lg:w-64">
-              <p className="text-xs font-light text-white">
-                You are connected to the Wrong Chain.{" "}
-                <span
-                  className="text-primary transition-opacity hover:cursor-pointer hover:opacity-80"
-                  onClick={() => switchNetwork && switchNetwork(activeChain)}
-                >
-                  Switch network
-                </span>
-              </p>
-            </div>
+  return (
+    <>
+      {kind === "opacity" ? (
+        <OpacityButton
+          data-disabled={!isMatch}
+          ref={refs.setReference}
+          onClick={handleClick}
+          disabled={disabled || busy}
+          busy={busy}
+          {...rest}
+          {...getReferenceProps()}
+        >
+          {children}
+        </OpacityButton>
+      ) : (
+        <ScaleButton
+          data-disabled={!isMatch}
+          ref={refs.setReference}
+          onClick={handleClick}
+          disabled={disabled || busy}
+          busy={busy}
+          {...rest}
+          {...getReferenceProps()}
+        >
+          {children}
+        </ScaleButton>
+      )}
+      {isMounted && (
+        <div ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
+          <FloatingArrow ref={arrowRef} style={styles} context={context} fill="#FF0083" />
+          <div style={styles} className="w-[70vw] border border-primary bg-component  p-middle lg:w-64">
+            <p className="text-xs font-light text-white">
+              You are connected to the Wrong Chain.{" "}
+              <span
+                className="text-primary transition-opacity hover:cursor-pointer hover:opacity-80"
+                onClick={() => switchNetwork && switchNetwork(activeChain)}
+              >
+                Switch network
+              </span>
+            </p>
           </div>
-        )}
-      </>
-    );
-  }
-);
+        </div>
+      )}
+    </>
+  );
+});
