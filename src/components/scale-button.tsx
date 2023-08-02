fce443d2838f0e4a485b7ca911e0042dfa578c30
JayJay1024 <lygm1062@gmail.com>
2023-08-02 16:30:28 +0800

    ensure match network button

--- src/components/scale-button.tsx
+++ src/components/scale-button.tsx
@@ -6,7 +6,7 @@ export default forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElem
 ) {
   return (
     <button
-      className={`transition-transform hover:scale-105 active:scale-95 disabled:scale-100 disabled:cursor-not-allowed disabled:opacity-60 ${className}`}
+      className={`transition-transform hover:scale-105 active:scale-95 disabled:scale-100 disabled:cursor-not-allowed disabled:opacity-60 data-[disabled=true]:cursor-not-allowed data-[disabled=true]:opacity-60 ${className}`}
       ref={ref}
       {...rest}
     >
