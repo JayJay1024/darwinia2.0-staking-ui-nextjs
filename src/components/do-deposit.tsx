6607b6ab31cbf7463ead76c81ee0ada3de22908b
JayJay1024 <lygm1062@gmail.com>
2023-08-03 17:47:36 +0800

    do deposit

--- src/components/do-deposit.tsx
+++ src/components/do-deposit.tsx
@@ -1,20 +1,72 @@
-import { calcKtonReward, formatBlanace, getChainConfig } from "@/utils";
+import { calcKtonReward, formatBlanace, getChainConfig, notifyTransaction } from "@/utils";
 import BalanceInput from "./balance-input";
 import DepositTermSelector from "./deposit-term-selector";
 import InputLabel from "./input-label";
 import { useAccount, useBalance } from "wagmi";
-import { useApp } from "@/hooks";
-import { useState } from "react";
+import { useApp, useStaking } from "@/hooks";
+import { useCallback, useState } from "react";
+import EnsureMatchNetworkButton from "./ensure-match-network-button";
+import { notification } from "./notification";
+import { writeContract, waitForTransaction } from "@wagmi/core";
 
 export default function DoDeposit() {
   const [depositRing, setDepositRing] = useState(0n);
   const [depositTerm, setDepositTerm] = useState(1);
+  const [busy, setBusy] = useState(false);
 
   const { address } = useAccount();
   const { data: balanceData } = useBalance({ address });
+  const { minimumDeposit } = useStaking();
   const { activeChain } = useApp();
 
-  const { nativeToken, ktonToken } = getChainConfig(activeChain);
+  const chainConfig = getChainConfig(activeChain);
+
+  const handleDeposit = useCallback(async () => {
+    if (depositRing < minimumDeposit) {
+      notification.warn({
+        description: `Please deposit at least ${formatBlanace(minimumDeposit, chainConfig.nativeToken.decimals, {
+          keepZero: false,
+        })} ${chainConfig.nativeToken.symbol}.`,
+      });
+      return;
+    } else if ((balanceData?.value || 0n) < depositRing) {
+      notification.warn({ description: "Your balance is insufficient." });
+      return;
+    } else {
+      setBusy(true);
+
+      try {
+        const contractAbi = (await import(`@/config/abi/${chainConfig.contract.deposit.abiFile}`)).default;
+
+        const { hash } = await writeContract({
+          address: chainConfig.contract.deposit.address,
+          abi: contractAbi,
+          functionName: "lock",
+          args: [depositRing, depositTerm],
+        });
+        const receipt = await waitForTransaction({ hash });
+
+        if (receipt.status === "success") {
+          setDepositRing(0n);
+          setDepositTerm(1);
+        }
+        notifyTransaction(receipt, chainConfig.explorer);
+      } catch (err) {
+        console.error(err);
+        notification.error({ description: (err as Error).message });
+      }
+
+      setBusy(false);
+    }
+  }, [
+    depositRing,
+    depositTerm,
+    minimumDeposit,
+    chainConfig.contract.deposit,
+    chainConfig.explorer,
+    chainConfig.nativeToken,
+    balanceData?.value,
+  ]);
 
   return (
     <div className="flex flex-col gap-middle bg-component p-5">
@@ -31,11 +83,12 @@ export default function DoDeposit() {
         <BalanceInput
           label="Amount"
           balance={balanceData?.value || 0n}
-          symbol={nativeToken.symbol}
-          decimals={nativeToken.decimals}
-          logoPath={nativeToken.logoPath}
+          symbol={chainConfig.nativeToken.symbol}
+          decimals={chainConfig.nativeToken.decimals}
+          logoPath={chainConfig.nativeToken.logoPath}
           className="lg:flex-1"
           onChange={setDepositRing}
+          reset={depositRing <= 0}
         />
 
         {/* deposit term */}
@@ -49,21 +102,25 @@ export default function DoDeposit() {
           <InputLabel label="Reward you'll receive" />
           <div className="flex h-10 items-center justify-between bg-primary px-middle">
             <span className="text-sm font-bold text-white">
-              {formatBlanace(calcKtonReward(depositRing, depositTerm), ktonToken?.decimals, { precision: 4 })}
+              {formatBlanace(calcKtonReward(depositRing, depositTerm), chainConfig.ktonToken?.decimals, {
+                precision: 6,
+              })}
             </span>
-            <span className="text-sm font-bold text-white">{ktonToken?.symbol}</span>
+            <span className="text-sm font-bold text-white">{chainConfig.ktonToken?.symbol}</span>
           </div>
         </div>
       </div>
 
       <div className="h-[1px] bg-white/20" />
 
-      <button
+      <EnsureMatchNetworkButton
         disabled={!(depositRing > 0)}
+        busy={busy}
         className="bg-primary px-large py-middle text-sm font-bold text-white transition-opacity hover:opacity-80 active:opacity-60 disabled:cursor-not-allowed disabled:opacity-60 lg:w-40"
+        onClick={handleDeposit}
       >
         Deposit
-      </button>
+      </EnsureMatchNetworkButton>
     </div>
   );
 }
