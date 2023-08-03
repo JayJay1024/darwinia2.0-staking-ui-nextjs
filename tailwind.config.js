1789c43059e8a420a47a163139cf08d0c8056486
JayJay1024 <lygm1062@gmail.com>
2023-08-03 11:01:08 +0800

    count-loading component

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         large: "0.9375rem", // 15px
       },
       screens: {
-        "xl": "1200px",
+        xl: "1200px",
         "2xl": "1200px",
       },
       keyframes: {
@@ -34,12 +34,30 @@ module.exports = {
         notificationfadeout: {
           "100%": { height: 0 },
         },
+        countloadingsmall: {
+          "0%": {
+            height: "16px",
+          },
+          "50%, 100%": {
+            height: "8px",
+          },
+        },
+        countloadinglarge: {
+          "0%": {
+            height: "32px",
+          },
+          "50%, 100%": {
+            height: "16px",
+          },
+        },
       },
       animation: {
         "notification-enter": "rightenter 400ms ease-out",
         "notification-leave": "rightleave 400ms ease-out forwards",
         "notification-fadeout": "notificationfadeout 200ms ease-out forwards",
-      }
+        "count-loading-small": "countloadingsmall 1200ms cubic-bezier(0, 0.5, 0.5, 1) infinite",
+        "count-loading-large": "countloadinglarge 1200ms cubic-bezier(0, 0.5, 0.5, 1) infinite",
+      },
     },
   },
   plugins: [],
