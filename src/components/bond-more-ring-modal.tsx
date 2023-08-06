7ea6ad39f589d3c8c27b835ca7072138153c846a
JayJay1024 <lygm1062@gmail.com>
2023-08-06 13:05:43 +0800

    bond more ring

--- src/components/bond-more-ring-modal.tsx
+++ src/components/bond-more-ring-modal.tsx
@@ -1,9 +1,10 @@
-import { getChainConfig } from "@/utils";
+import { getChainConfig, notifyTransaction } from "@/utils";
 import BondMoreTokenModal from "./bond-more-token-modal";
-import { ChainID } from "@/types";
-import { parseEther } from "viem";
-
-const { nativeToken } = getChainConfig(ChainID.CRAB);
+import { useAccount, useBalance } from "wagmi";
+import { useApp, useStaking } from "@/hooks";
+import { useCallback, useState } from "react";
+import { notification } from "./notification";
+import { writeContract, waitForTransaction } from "@wagmi/core";
 
 export default function BondMoreRingModal({
   isOpen,
@@ -12,16 +13,59 @@ export default function BondMoreRingModal({
   isOpen: boolean;
   onClose?: () => void;
 }) {
+  const { activeChain } = useApp();
+  const { address } = useAccount();
+  const { data: ringBalance } = useBalance({ address });
+  const { calcExtraPower } = useStaking();
+
+  const [inputAmount, setInputAmount] = useState(0n);
+  const [busy, setBusy] = useState(false);
+
+  const { nativeToken, contract, explorer } = getChainConfig(activeChain);
+
+  const handleBond = useCallback(async () => {
+    if ((ringBalance?.value || 0n) < inputAmount) {
+      notification.warn({ description: "Your balance is insufficient." });
+    } else {
+      setBusy(true);
+
+      try {
+        const contractAbi = (await import(`@/config/abi/${contract.staking.abiFile}`)).default;
+
+        const { hash } = await writeContract({
+          address: contract.staking.address,
+          abi: contractAbi,
+          functionName: "stake",
+          args: [inputAmount, 0n, []],
+        });
+        const receipt = await waitForTransaction({ hash });
+
+        if (receipt.status === "success") {
+          onClose();
+        }
+        notifyTransaction(receipt, explorer);
+      } catch (err) {
+        console.error(err);
+      }
+
+      setBusy(false);
+    }
+  }, [contract.staking, explorer, inputAmount, ringBalance?.value, onClose]);
+
   return (
     <BondMoreTokenModal
       isOpen={isOpen}
       symbol={nativeToken.symbol}
       decimals={nativeToken.decimals}
-      power={0n}
-      balance={parseEther("12345.87654")}
+      power={calcExtraPower(inputAmount, 0n)}
+      balance={ringBalance?.value || 0n}
+      busy={busy}
+      disabled={inputAmount <= 0n}
+      isReset={inputAmount <= 0}
       onClose={onClose}
-      onBond={onClose}
+      onBond={handleBond}
       onCancel={onClose}
+      onChange={setInputAmount}
     />
   );
 }
