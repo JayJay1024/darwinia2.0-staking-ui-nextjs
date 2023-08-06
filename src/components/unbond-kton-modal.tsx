ac35efeda2e1a3dac41104930749bbfca8fbc3f5
JayJay1024 <lygm1062@gmail.com>
2023-08-06 14:54:54 +0800

    unbond kton

--- src/components/unbond-kton-modal.tsx
+++ src/components/unbond-kton-modal.tsx
@@ -1,9 +1,9 @@
-import { getChainConfig } from "@/utils";
-import { ChainID } from "@/types";
-import { parseEther } from "viem";
+import { formatBlanace, getChainConfig, notifyTransaction } from "@/utils";
 import UnbondTokenModal from "./unbond-token-modal";
-
-const { ktonToken } = getChainConfig(ChainID.CRAB);
+import { useApp, useStaking } from "@/hooks";
+import { useCallback, useState } from "react";
+import { notification } from "./notification";
+import { writeContract, waitForTransaction } from "@wagmi/core";
 
 export default function UnbondKtonModal({
   isOpen,
@@ -12,17 +12,64 @@ export default function UnbondKtonModal({
   isOpen: boolean;
   onClose?: () => void;
 }) {
+  const { activeChain } = useApp();
+  const { stakedKton, calcExtraPower } = useStaking();
+
+  const [inputAmount, setInputAmount] = useState(0n);
+  const [busy, setBusy] = useState(false);
+
+  const { ktonToken, contract, explorer } = getChainConfig(activeChain);
+
+  const handleUnbond = useCallback(async () => {
+    if (stakedKton < inputAmount) {
+      notification.warn({
+        description: `You can't unbond more than ${formatBlanace(stakedKton, ktonToken?.decimals, {
+          precision: 4,
+          keepZero: false,
+        })} ${ktonToken?.symbol}`,
+      });
+    } else {
+      setBusy(true);
+
+      try {
+        const contractAbi = (await import(`@/config/abi/${contract.staking.abiFile}`)).default;
+
+        const { hash } = await writeContract({
+          address: contract.staking.address,
+          abi: contractAbi,
+          functionName: "unstake",
+          args: [0n, inputAmount, []],
+        });
+        const receipt = await waitForTransaction({ hash });
+
+        if (receipt.status === "success") {
+          onClose();
+        }
+        notifyTransaction(receipt, explorer);
+      } catch (err) {
+        console.error(err);
+        notification.error({ description: (err as Error).message });
+      }
+
+      setBusy(false);
+    }
+  }, [explorer, contract.staking, stakedKton, inputAmount, ktonToken, onClose]);
+
   return (
     ktonToken && (
       <UnbondTokenModal
         isOpen={isOpen}
         symbol={ktonToken.symbol}
         decimals={ktonToken.decimals}
-        power={0n}
-        balance={parseEther("12345.87654")}
+        power={calcExtraPower(0n, inputAmount)}
+        balance={stakedKton}
+        busy={busy}
+        disabled={inputAmount <= 0n}
+        isReset={inputAmount <= 0}
         onClose={onClose}
-        onBond={onClose}
+        onUnbond={handleUnbond}
         onCancel={onClose}
+        onChange={setInputAmount}
       />
     )
   );
